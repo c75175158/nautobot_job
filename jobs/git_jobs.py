48af6b51d2573953479eab5898843277bd24e071
c75175158 <c75175158@gmail.com>
2025-10-28 13:35:01 -0400

    Test job

--- jobs/git_jobs.py
+++ jobs/git_jobs.py
@@ -5,7 +5,7 @@ class MyFirstJob(Job):
     name = "My First Nautobot Job"
     description = "A simple example job."
 
-    def run(self, data, commit):
+    def run(self):
         self.log_info(message="Hello from My First Nautobot Job!")
         return "Job completed successfully."
 
