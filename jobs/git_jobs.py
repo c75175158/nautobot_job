69734644577e40577578ace05d7516d6021ec926
c75175158 <c75175158@gmail.com>
2025-10-28 13:03:28 -0400

    Test job

---
+++ jobs/git_jobs.py
@@ -0,0 +1,19 @@
+from nautobot.apps.jobs import Job, register_jobs
+
+# new
+name = "Hello World Nautobot Jobs"
+
+
+class HelloJobs(Job):
+    # new
+    class Meta:
+        name = "Hello Jobs"
+        description = "Hello World for first Nautobot Jobs"
+
+    def run(self):
+        self.logger.debug("Hello, this is my first Nautobot Job.")
+
+
+register_jobs(
+    HelloJobs,
+)
