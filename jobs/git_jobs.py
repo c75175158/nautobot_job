812e5306f6aec5fe3d28d049f80f7e2dbe170ac9
c75175158 <c75175158@gmail.com>
2025-10-31 11:45:31 -0400

    Test job

--- jobs/git_jobs.py
+++ jobs/git_jobs.py
@@ -1,14 +1,38 @@
-from nautobot.apps.jobs import Job, register_jobs
+from nautobot.apps.jobs import Job, register_jobs, FileVar
+from nautobot.dcim.models import Device, Location, DeviceType
+from nautobot.extras.models import Role, Status
 
 
-class MyFirstJob(Job):
-    name = "My First Nautobot Job"
-    description = "A simple example job."
+class ImportLocationTypes(Job):
+    class Meta:
+        name = "CSV File Upload and Process"
+        description = "Please select a CSV file for upload"
 
-    def run(self):
-        self.logger.info(f"Hello from My First Nautobot Job!")
-        return "Job completed successfully."
+    file = FileVar(
+        description="CSV File to upload",
+    )
+
+    def run(self, file):
+        file_contents = file.read().decode("utf-8")
+        self.logger.info(file_contents)
+        lines = file_contents.splitlines()
+        self.logger.info(lines)
+
+        self.logger.info("Parsing of the lines")
+
+        for line in lines[1:]:
+            device_name, role_name, model_name, location_name = line.split(",")
+            # test = Location.location_type.create(name=location_name)
+
+            self.logger.info(f"Name: {device_name}")
+            self.logger.info(f"Role: {role_name}")
+            self.logger.info(f"Device type: {model_name}")
+            self.logger.info(f"Location: {location_name}")
+
+
+        return "Execution completed"
+
 
 register_jobs(
-    MyFirstJob,
+    ImportLocationTypes,
 )
