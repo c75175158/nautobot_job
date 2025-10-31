fdec4f035f0f1a91ca319a28ace1db975047537f
c75175158 <c75175158@gmail.com>
2025-10-31 15:02:10 -0400

    Test job

--- jobs/git_jobs.py
+++ jobs/git_jobs.py
@@ -30,16 +30,19 @@ class ImportLocationTypes(Job):
             name, description, content_type, ne_stable, parent_type, parent_descr = line.split(",")
             # test = Location.location_type.create(name=location_name)
 
-            payload ={
-                "name":  name,
-                "parent": parent_type if parent_type != 'NoObject' else None,
-                "nestable": convert[ne_stable],
-            }
+            try:
+                payload =  {
+                    "name":  name,
+                    "parent": parent_type if parent_type != 'NoObject' else None,
+                    "nestable": convert[ne_stable],
+                }
 
-            self.logger.info(payload)
+                self.logger.info(payload)
 
-            if not LocationType.objects.filter(name=parent_type).exists():
                 LocationType.objects.create(**payload)
+
+            except Exception as e:
+                pass
 
             # self.logger.info(f"State: {name}")
             # self.logger.info(f"City: {description}")
@@ -47,8 +50,6 @@ class ImportLocationTypes(Job):
             # self.logger.info(f"Branch: {ne_stable}")
             # self.logger.info(f"Branch: {parent_type}")
             # self.logger.info(f"Branch: {parent_descr}")
-
-        return "Execution completed"
 
 
 register_jobs(
