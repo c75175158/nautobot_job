43318f5543eea1517564782b338990521d8662ce
c75175158 <c75175158@gmail.com>
2025-10-31 12:32:02 -0400

    Test job

--- jobs/git_jobs.py
+++ jobs/git_jobs.py
@@ -25,15 +25,27 @@ class ImportLocationTypes(Job):
 
             self.logger.info(line)
 
-            name, description, type, ne_stable, parent_type, parent_descr = line.split(",")
+            name, description, content_type, ne_stable, parent_type, parent_descr = line.split(",")
             # test = Location.location_type.create(name=location_name)
 
-            self.logger.info(f"State: {name}")
-            self.logger.info(f"City: {description}")
-            self.logger.info(f"DC type: {type}")
-            self.logger.info(f"Branch: {ne_stable}")
-            self.logger.info(f"Branch: {parent_type}")
-            self.logger.info(f"Branch: {parent_descr}")
+            payload ={
+                "name":  name,
+                "slug": name,
+                "parent": parent_type,
+                "nestable": ne_stable,
+                "content_types": content_type.split(',')
+            }
+
+            Location.objects.create(**payload)
+
+            self.logger.info(Location.objects.get(name=name))
+
+            # self.logger.info(f"State: {name}")
+            # self.logger.info(f"City: {description}")
+            # self.logger.info(f"DC type: {type}")
+            # self.logger.info(f"Branch: {ne_stable}")
+            # self.logger.info(f"Branch: {parent_type}")
+            # self.logger.info(f"Branch: {parent_descr}")
 
         return "Execution completed"
 
