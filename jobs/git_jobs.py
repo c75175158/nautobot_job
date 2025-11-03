9f808b7fe03f56b93a71e0b25a9a294bf9d495f6
c75175158 <c75175158@gmail.com>
2025-11-03 09:52:26 -0500

    Test job

--- jobs/git_jobs.py
+++ jobs/git_jobs.py
@@ -55,7 +55,7 @@ class ImportLocationTypes(Job):
 
                     payload = {
                         "name": contents[0],
-                        "parent": parent_type,
+                        "parent": parent_obj,
                         "nestable": convert[ne_stable],
                     }
 
