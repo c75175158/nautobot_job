fed7affda1026a28166dfbaca34197d6cad7d4fc
c75175158 <c75175158@gmail.com>
2025-11-04 13:12:02 -0500

    Test job

--- jobs/git_jobs.py
+++ jobs/git_jobs.py
@@ -118,7 +118,7 @@ class ImportLocation(Job):
 
             parent = Location.objects.get(name=location[0])
 
-            if Location.objects.filter(name=location[2]).count() <= 1:
+            if Location.objects.filter(name=location[2]).count() < 1:
 
                 Location.objects.create(
                     name=location[2],
@@ -128,7 +128,7 @@ class ImportLocation(Job):
 
                 )
 
-            if Location.objects.filter(name=location[1]).count() <= 1:
+            if Location.objects.filter(name=location[1]).count() < 1:
                 Location.objects.create(
                     name=location[1] ,
                     parent=Location.objects.get(name=location[2]),
